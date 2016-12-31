6c92cefbe66ea6784f3cda128c0a868cf3d92853
Simone Stefani <s.stefani95@gmail.com>
2016-12-31 18:54:35 +0100

    Add quicksort

---
+++ Sorting/src/main/java/Quick.java
@@ -0,0 +1,41 @@
+import edu.princeton.cs.introcs.StdRandom;
+
+/**
+ * Created by S. Stefani on 2016-12-31.
+ */
+public class Quick extends Sort {
+    public static void main(String[] args) {
+        Integer[] a = {6, 3, 8, 9, 2, 1, 4, 5, 7};
+        show(a);
+        sort(a);
+        assert isSorted(a);
+        show(a);
+    }
+
+    public static void sort(Comparable[] a) {
+        StdRandom.shuffle(a);
+        sort(a, 0, a.length - 1);
+    }
+
+    private static void sort(Comparable[] a, int lo, int hi) {
+        if (hi <= lo) return;
+        int j = partition(a, lo, hi);
+        sort(a, lo, j-1);
+        sort(a, j+1, hi);
+    }
+
+    private static int partition(Comparable[] a, int lo, int hi) {
+        int i = lo;
+        int j = hi + 1;
+        Comparable v = a[lo];
+
+        while(true) {
+            while (less(a[++i], v)) if (i == hi) break;
+            while (less(v, a[--j])) if (j == lo) break;
+            if (i >= j) break;
+            exch(a, i, j);
+        }
+        exch(a, lo, j);
+        return j;
+    }
+}
