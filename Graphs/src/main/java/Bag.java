9bd332d930dcac7d5898893ca13a0992ba47c1a5
Simone Stefani <s.stefani95@gmail.com>
2017-01-06 10:59:52 +0100

    Add basic graph

---
+++ Graphs/src/main/java/Bag.java
@@ -0,0 +1,47 @@
+import java.util.Iterator;
+
+/**
+ * Bag with linked list for graphs
+ *
+ * Created by S. Stefani on 2017-01-06.
+ */
+
+public class Bag<Item> implements Iterable<Item> {
+    private Node first;
+    private int N = 0;
+
+    public void add(Item item) {
+        Node oldfirst = first;
+        first = new Node();
+        first.item = item;
+        first.next = oldfirst;
+        N++;
+    }
+
+    public int size() {
+        return N;
+    }
+
+    private class Node {
+        Item item;
+        Node next;
+    }
+
+    public Iterator<Item> iterator() {
+        return new ListIterator();
+    }
+
+    private class ListIterator implements Iterator<Item> {
+        private Node current = first;
+
+        public boolean hasNext() {
+            return current != null;
+        }
+
+        public Item next() {
+            Item item = current.item;
+            current = current.next;
+            return item;
+        }
+    }
+}
